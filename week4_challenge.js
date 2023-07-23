c5d68270854f7dd8a8576b3c72e90eea7d9fa762
chrishahn <chrishahn97@outlookcom>
2023-07-23 11:25:00 -0500

    done with week 4

--- week4_challenge.js
+++ week4_challenge.js
@@ -1,1 +1,11 @@
+function addUp(num) {
+  let sum = 0;
+  for (let i = 1; i <= num; i++) {
+    sum += i;
+  }
+  return sum;
+}
 
+console.log(addUp(4));
+console.log(addUp(13));
+console.log(addUp(600));
